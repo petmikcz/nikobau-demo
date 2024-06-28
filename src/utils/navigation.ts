303b5e12a962aeadd56f1662a608681ab6ba05f3
petmikcz <petmik81@gmail.com>
2024-06-28 02:04:35 +0200

    First commit

---
+++ src/utils/navigation.ts
@@ -0,0 +1,31 @@
+// An array of links for navigation bar
+const navBarLinks = [
+  { name: "Domů", url: "/" },
+  { name: "Realizace", url: "/#" },
+  { name: "Služby", url: "/#" },
+  { name: "Kontakt", url: "/contact" },
+];
+// An array of links for footer
+const footerLinks = [
+    {
+    section: "Společnost",
+    links: [
+      { name: "O nás", url: "#" },
+      { name: "Blog", url: "/blog" },
+      { name: "Kontakt", url: "#" },
+    ],
+  },
+];
+// An object of links for social icons
+const socialLinks = {
+  facebook: "https://www.facebook.com/",
+  github: "https://github.com/mearashadowfax/ScrewFast",
+  google: "https://www.google.com/",
+  slack: "https://slack.com/",
+};
+
+export default {
+  navBarLinks,
+  footerLinks,
+  socialLinks,
+};
